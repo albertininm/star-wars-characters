8e4d8cd825f2988eb2ce6e3f8c6ee05e3e45851f
Albertinin Mourato Santos <albertinin.mourato@liferay.com>
2022-10-11 00:27:34 +0200

    Add index to group hooks exports

---
+++ src/hooks/index.ts
@@ -0,0 +1,5 @@
+export {default as useDebounce} from './useDebounce';
+export {default as useFetch} from './useFetch';
+export {default as useFetchFilms} from './useFetchFilms';
+export {default as useFetchPeople} from './useFetchPeople';
+export {default as useFetchPlanets} from './useFetchPlanets';

ddcfe9f49f704b857d6d14037b12d912f4a47d4e
Albertinin Mourato Santos <albertinin.mourato@liferay.com>
2022-10-12 00:31:00 +0200

    Transform css to sass

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,26 @@
+import People from 'components/People/People';
 import React from 'react';
-import './App.css';
+import { useState } from 'react';
+import './App.scss';
+import './globals.scss';
+import { useFetchPeople } from './hooks';
 
 function App() {
+  const [inputText, setInputText] = useState('');
+
+  const { results: people } = useFetchPeople(inputText);
+
+  console.log(people);
+
   return (
     <div className="App">
-      <body>App Body</body>
+      <div>App Body</div>
+      <input onChange={(e) => setInputText(e.target.value)} type="text"></input>
+      <div className="characters-wrapper">
+        {people.map((item) => (
+          <People {...item} key={item.name} />
+        ))}
+      </div>
     </div>
   );
 }

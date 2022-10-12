aa38fb4208477d7a9834d1be56e29804f0eaf965
Albertinin Mourato Santos <albertinin.mourato@liferay.com>
2022-10-13 01:54:33 +0200

    Implement Movie component

--- src/components/Movie/Movie.tsx
+++ src/components/Movie/Movie.tsx
@@ -1,7 +1,20 @@
 import React from 'react';
+import './Movie.scss';
 
 const Movie: React.FC = () => {
-  return <>CRANCKY</>;
+  return (
+    <div className="movie">
+      <div className="release-year">1997</div>
+      <div className="title">A New Hope</div>
+      <div className="details">
+        <div className="release-info">
+          <div className="release-info__title">Release date</div>
+          <div className="release-info__date">May 27, 1997</div>
+        </div>
+        <div className="sinopse">It is a period of civil war.Rebel spaceships, strikingfrom a hidden base, have wontheir first victory againstthe evil Galactic Empire.During the battle...</div>
+      </div>
+    </div>
+  );
 };
 
 export default Movie;

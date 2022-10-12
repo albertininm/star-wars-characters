1064feeeaeb7823b830e8bae7b6e8dff2c20ef80
Albertinin Mourato Santos <albertinin.mourato@liferay.com>
2022-10-12 22:54:02 +0200

    Improve UI of Badge component

--- src/components/Badge/Badge.tsx
+++ src/components/Badge/Badge.tsx
@@ -1,19 +1,18 @@
 import React, { PropsWithChildren } from 'react';
 import cx from 'classnames';
 import './Badge.scss';
-import { getNumberFromString, HashableColors } from 'helpers/colors';
+import { getColorFromText, Colors } from 'helpers/colors';
 
 interface BadgeProps {
   className?: string;
   content: string;
+  color?: typeof Colors[number];
 }
 
-const Badge: React.FC<PropsWithChildren<BadgeProps>> = ({className, content}) => {
-  const colorIndex = getNumberFromString(content);
+const Badge: React.FC<PropsWithChildren<BadgeProps>> = ({className, content, color}) => {
+  const colorClassName = color || getColorFromText(content);
 
-  console.log(colorIndex);
-
-  const classes = cx('badge', 'truncate', HashableColors[colorIndex], className);
+  const classes = cx('badge', 'truncate', colorClassName, className);
 
   return <div className={classes} title={content}>{content}</div>;
 };

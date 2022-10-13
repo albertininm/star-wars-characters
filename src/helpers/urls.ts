de176c0475112c20eb7e8905efdb3fd8a4b7c9db
Albertinin Mourato Santos <albertinin.mourato@liferay.com>
2022-10-13 23:38:25 +0200

    Fix API endpoints

--- src/helpers/urls.ts
+++ src/helpers/urls.ts
@@ -1,10 +1,11 @@
 import config from '../config/app.config';
 
-type Collections = 'people' | 'films' | 'planets';
+type Collections = 'people' | 'film' | 'planet' | 'specie';
 
 export const apiUrls: Record<Collections, (id: string) => string> = {
   people: (peopleId) => `${config.baseApiEndpoint}/people/?search=${peopleId}`,
-  films: (filmId) => `${config.baseApiEndpoint}/films/?search=${filmId}`,
-  planets: (planetId) =>
-    `${config.baseApiEndpoint}/planets/?search=${planetId}`,
+  specie: (specieId) => `${config.baseApiEndpoint}/species/${specieId}`,
+  film: (filmId) => `${config.baseApiEndpoint}/films/${filmId}`,
+  planet: (planetId) =>
+    `${config.baseApiEndpoint}/planets/${planetId}`,
 };

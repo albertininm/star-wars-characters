1dd74fec1b2e7d433802bfbc23ea40c4f73de400
Albertinin Mourato Santos <albertinin.mourato@liferay.com>
2022-10-14 18:59:07 +0200

    Use cache strategy for SpecieItem

--- src/components/Character/SpeciesInfo/SpecieItem/SpecieItem.tsx
+++ src/components/Character/SpeciesInfo/SpecieItem/SpecieItem.tsx
@@ -1,4 +1,5 @@
 import Badge from 'components/Badge/Badge';
+import useDataContext from '../../../../contexts/CacheContext/useCacheContext';
 import {useFetch} from 'hooks';
 import React from 'react';
 import { Specie } from 'types';
@@ -14,13 +15,23 @@ interface SpecieItemInterface extends React.FC<SpecieItemProps> {
 const UnknownSpecie = () => <Badge content="Unknown" color="gray" />;
 
 const SpecieItem: SpecieItemInterface = ({url}) => {
-  const {loading, data} = useFetch<Specie>(url);
+  const {species, setSpecie} = useDataContext();
+
+  const cachedSpecie = species[url];
+
+  const {loading, data} = useFetch<Specie>({url, 
+    skip: Boolean(cachedSpecie),
+    onRequestFinish: (specie) => {
+      if(!cachedSpecie) {
+        setSpecie({specie, url});
+      }
+    }});
 
   if(loading) {
     return <Badge.Skeleton />;
   }
 
-  return <Badge content={data?.name || 'CRANCKY'} />;
+  return <Badge content={cachedSpecie?.name || data?.name} />;
 };
 
 SpecieItem.Unknown = UnknownSpecie;

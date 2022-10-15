e4e1c1a71a4fb89dc0732141db4e1fb47a851f47
Albertinin Mourato Santos <albertinin.mourato@liferay.com>
2022-10-16 01:09:49 +0200

    Implement helper to parse synopsis

--- src/helpers/string.ts
+++ src/helpers/string.ts
@@ -3,3 +3,7 @@ export const getInitialsFromName = (name: string) => {
   
   return `${firstName?.[0] ?? ''}${secondName?.[0] ?? ''}`;
 };
+
+export const getFormattedSynopsis = (synopsis: string) => {
+  return synopsis.replace(/(\r\n|\n|\r)/gm, '').split('').slice(0, 151).join('');
+};

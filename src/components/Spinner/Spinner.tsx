9163a06c006266943a21664bcf533ce738b1e513
Albertinin Mourato Santos <albertinin.mourato@liferay.com>
2022-10-15 22:52:58 +0200

    Create spinner component

---
+++ src/components/Spinner/Spinner.tsx
@@ -0,0 +1,15 @@
+import cx from 'classnames';
+import React from 'react';
+import './Spinner.scss';
+
+interface SpinnerProps {
+  className?: string;
+}
+
+const Spinner: React.FC<SpinnerProps> = ({className}) => {
+  const classes = cx('spinner', className);
+
+  return <div className={classes}></div>;
+};
+
+export default Spinner;

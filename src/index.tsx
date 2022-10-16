a78b402ba9d57967e09882f8cebad3cbe61665a4
Albertinin Mourato Santos <albertinin.mourato@liferay.com>
2022-10-16 20:05:44 +0200

    Move css definition for fonts to App.scss

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import DataContextProvider from 'contexts/CacheContext/CacheContext';
 

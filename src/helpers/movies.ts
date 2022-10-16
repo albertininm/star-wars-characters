dc3c8b2f737ccd9c12f4c382465f832aa74f8e4e
Albertinin Mourato Santos <albertinin.mourato@liferay.com>
2022-10-16 04:56:25 +0200

    Implement helper to sort movies by release date

--- src/helpers/movies.ts
+++ src/helpers/movies.ts
@@ -1,6 +1,13 @@
 import {Movie} from 'types';
 import {SortType} from 'components/MoviesSection/MoviesSection';
 
-const sortMovies = ({movies, sortType}: {movies: Movie[]; sortType: SortType}) => {
-  console.log('a');
+export const sortMovies = ({movies, sortType = 'newest'}: {movies: Movie[]; sortType?: SortType}) => {
+  movies.sort((m1, m2) => {
+    const milliseconds1 = new Date(m1.release_date).getTime();
+    const milliseconds2 = new Date(m2.release_date).getTime();
+
+    return sortType === 'newest' ? milliseconds2 - milliseconds1 : milliseconds1 - milliseconds2;
+  });
+
+  return movies;
 };

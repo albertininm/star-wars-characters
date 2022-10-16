6aae94127ed9f018313aa979f242dcc4fd1b28c0
Albertinin Mourato Santos <albertinin.mourato@liferay.com>
2022-10-16 04:58:51 +0200

    Integrate page with character and movie sections

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,23 @@
 
-import Character from 'components/Character/Character';
+import Character from 'components/CharactersSection/Character/Character';
 import React from 'react';
 import { useState } from 'react';
 import './App.scss';
 import './globals.scss';
 import { useFetchPeople } from './hooks';
-import { People } from 'types';
+import { Movie, People } from 'types';
 import SearchInput from 'components/SearchInput/SearchInput';
 import AppBackground from 'components/AppBackground/AppBackground';
-import LoadingCharactersPlaceholder from 'components/LoadingCharactersPlaceholder/LoadingCharactersPlaceholder';
+import LoadingCharactersPlaceholder from 'components/LoadingContentPlaceholder/LoadingContentPlaceholder';
 import NoCharacterFoundPlaceholder from 'components/NoCharacterFoundPlaceholder/NoCharacterFoundPlaceholder';
+import useFetchMovies from 'hooks/useFetchMovies';
+import MoviesSection from 'components/MoviesSection/MoviesSection';
 
 function App() {
   const [inputText, setInputText] = useState('');
+
+  const [movies, setMovies] = useState<Movie[]>([]);
+  const {loading: loadingMovies, fetchMovies} = useFetchMovies();
 
   const { results: characters = [], loading } = useFetchPeople(inputText);
   const [selectedCharacter, setSelectedCharacter] = useState<People>();
@@ -22,45 +27,52 @@ function App() {
       <AppBackground />
       <div className="app-layout">
         <div className="app-content">
-          <div className="character-search">
-            <SearchInput
-              onChange={(e) => {
-                const searchText = e.target.value;
+          <div className="characters-section">
+            <div className="section-title">
+              <h1 className="title">Star Wars</h1>
+              <h1 className="subtitle">Characters Wiki</h1>
+            </div>
+            <div className="character-search">
+              <SearchInput
+                onChange={(e) => {
+                  const searchText = e.target.value;
 
-                setInputText(searchText);
-                setSelectedCharacter(undefined);
-              }}
-            />
+                  setInputText(searchText);
+                  setSelectedCharacter(undefined);
+                  setMovies([]);
+                }}
+              />
+            </div>
+            <div className="characters-wrapper">
+              {loading? 
+                <LoadingCharactersPlaceholder contentName="characters" />
+                :
+                (characters.length === 0 ? <NoCharacterFoundPlaceholder /> :
+                  characters.map(character => 
+                    <Character
+                      key={character.name}
+                      name={character.name}
+                      homeWorldUrl={character.homeworld}
+                      onClick={async () => {
+                        setSelectedCharacter(character);
+                        const fetchedMovies = await fetchMovies(character.films);
+                        setMovies(fetchedMovies);
+                      }}
+                      speciesUrls={character.species}
+                    />)
+                )
+              }
+            </div>
           </div>
-
-          <div className="characters-wrapper">
-            {loading? 
-              <LoadingCharactersPlaceholder />
-              :
-
-              (characters.length === 0 ? <NoCharacterFoundPlaceholder /> :
-                characters.map(character => 
-                  <Character
-                    key={character.name}
-                    name={character.name}
-                    homeWorldUrl={character.homeworld}
-                    onClick={() => setSelectedCharacter(character)}
-                    speciesUrls={character.species}
-                  />)
-              )
-            }
-          </div>
+          <MoviesSection
+            movies={movies}
+            skeleton={loadingMovies}
+            skeletonNumberOfItems={selectedCharacter?.films.length ?? 0}
+          />
         </div>
       </div>
     </div>
   );
 }
-
-{/* <Character name='Yoda' homeWorldUrl='https://swapi.dev/api/planets/9/' speciesUrls={['https://swapi.dev/api/species/1/', 'https://swapi.dev/api/species/2/']}/> */}
-{/* <Movie
-      releaseDate="1997-05-25"
-      title="A New Hope"
-      synopsis="It is a period of civil war.Rebel spaceships, strikingfrom a hidden base, have wontheir first victory againstthe evil Galactic Empire.During the battle..."
-    /> */}
 
 export default App;

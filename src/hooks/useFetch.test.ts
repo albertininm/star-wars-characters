8267322bbbd5439fd80e0eee239d57c5ba036c89
Albertinin Mourato Santos <albertinin.mourato@liferay.com>
2022-10-17 12:08:07 +0200

    Add tests for useFetch

---
+++ src/hooks/useFetch.test.ts
@@ -0,0 +1,62 @@
+import {renderHook} from '@testing-library/react-hooks';
+import useFetch from './useFetch';
+import { waitFor } from '@testing-library/react';
+import { act } from 'react-dom/test-utils';
+
+describe('Hooks: useFetch', () => {
+  it('should return "loading" as true while request is not finished', async () => {
+    let requestResolver: (value: Response | PromiseLike<Response>) => void = () => null;
+    const url = 'https://challenger.api';
+
+    fetchMock.mockReturnValue(new Promise((resolve) => requestResolver = resolve));
+
+    await act(async () => {
+      const {result} = renderHook(() => useFetch({url}));
+  
+      await waitFor(() => {
+        expect(result.current.loading).toStrictEqual(true);
+      });
+      
+      requestResolver({} as unknown as Response);
+      
+      await waitFor(() => {
+        expect(result.current.loading).toStrictEqual(false);
+      });
+    });
+  });
+
+  it('should call "onRequestFinish" when request is finished', async () => {
+    const onRequestFinishMock = jest.fn();
+
+    let requestResolver: (value: Response | PromiseLike<Response>) => void = () => null;
+    const url = 'https://challenger.api';
+
+    fetchMock.mockReturnValue(new Promise((resolve) => requestResolver = resolve));
+
+    await act(async () => {
+      const {result} = renderHook(() => useFetch({url, onRequestFinish: onRequestFinishMock}));
+  
+      await waitFor(() => {
+        expect(result.current.loading).toStrictEqual(true);
+        expect(onRequestFinishMock).not.toHaveBeenCalled();
+      });
+      
+      requestResolver({json: () => true} as unknown as Response);
+      
+      await waitFor(() => {
+        expect(result.current.loading).toStrictEqual(false);
+        expect(onRequestFinishMock).toHaveBeenCalledTimes(1);
+      });
+    });
+  });
+
+  it('should not run request if "skip" is "true"', async () => {
+    const url = 'https://challenger.api';
+
+    fetchMock.mockReturnValue(new Promise((resolve) => resolve));
+
+    renderHook(() => useFetch({url, skip: true}));
+
+    expect(fetchMock.mock.calls).toHaveLength(0);
+  });
+});
